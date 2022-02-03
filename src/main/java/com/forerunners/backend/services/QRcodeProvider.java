fd558a80117dd2a0c95b611f7d8256d9b7e68441
Gaurav <Aniketan.Gaurav@sabre.com>
2022-02-03 10:52:50 +0530

    cloud deploy

--- src/main/java/com/forerunners/backend/services/QRcodeProvider.java
+++ src/main/java/com/forerunners/backend/services/QRcodeProvider.java
@@ -1,4 +1,16 @@
-package com.forerunners.backend.services;
-
-public class QRcodeProvider {
-}
+//package com.forerunners.backend.services;
+//
+//import java.io.IOException;
+//import java.nio.file.Path;
+//
+//public class QRcodeProvider {
+//
+//    public  static  void generateQRCodeImage(String text, int width, int height, String filePath)
+//        throws WriteerException, IOException {
+//
+//        QRcodeProvider qRcodeProvider = new QRcodeProvider();
+//        Bitmatrix bitmatrix = qRcodeProvider.encode(text, BarcodeFormat.QR_CODE, width, hegiht);
+//
+//        Path path = Fi
+//    }
+//}
